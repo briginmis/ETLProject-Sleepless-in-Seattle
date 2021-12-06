c77f156f7bef2f8c192cd49cf6378a7305672c4b
WaltonTan <walton.tan@hotmail.com>
2021-12-06 21:27:25 +0800

    Fixed cleaning functions.

--- Scripts/cleaning_functions.py
+++ Scripts/cleaning_functions.py
@@ -25,23 +25,22 @@ def explode_into_rows_in_new_table (input_df:pd.DataFrame,id_column, explode_col
     df2=df2.reset_index(drop=True)
     return df2
 
+def clean_dollar(input_df, col_list):
+    df=input_df.copy(deep=True)
+    for col in col_list:
+        df[col] = df[col].str.replace('$','')
+        df[col] = df[col].str.replace(',','')
+    return df
 
-def clean_all(df):
-    for x in range(len(df.columns)):
-        try:
-            if str.find(df.iloc[:, x].name,'url')>0: # if column header contains 'url' then skip column
-                z=1
-            else:
-                y = 0
-                while df.iloc[y, x]!=df.iloc[y, x]: # Check for NaN value and skip to next row to test
-                    y = y+1
-                if str.find(df.iloc[y, x],'$')>=0: # if column content contains '$' then remove the '$' and ','
-                    df.iloc[:, x] = df.iloc[:, x].str.replace('$','')
-                    df.iloc[:, x] = df.iloc[:, x].str.replace(',','')
-                elif str.find(df.iloc[y, x],'%')>0: # If column content contains '%' then remove '%'
-                    df.iloc[:, x] = df.iloc[:, x].str.replace('%','')
-                elif (len(df.iloc[y, x])==1) and (str.find(df.iloc[y, x],'t')>=0 or str.find(df.iloc[0, x],'f')>=0): # If column content is 't' or 'f' and length is 1 then change column to True or False
-                    df.iloc[:, x] = df.iloc[:, x].str.replace('t','True')
-                    df.iloc[:, x] = df.iloc[:, x].str.replace('f','False')
-        except:
-            z=1
+def clean_percent(input_df, col_list):
+    df=input_df.copy(deep=True)
+    for col in col_list:
+        df[col] = df[col].str.replace('%','')
+    return df
+
+def clean_boo(input_df, col_list):
+    df=input_df.copy(deep=True)
+    for col in col_list:
+        df[col] = df[col].str.replace('t','True')
+        df[col] = df[col].str.replace('f','False')
+    return df
